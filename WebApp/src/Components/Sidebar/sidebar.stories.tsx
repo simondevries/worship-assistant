5aff200be7e1c8a6b08b3815b909976fd6a8081a
Si <devries.simon94@gmail.com>
2021-10-29 21:44:42 +1300

    Added story

---
+++ WebApp/src/Components/Sidebar/sidebar.stories.tsx
@@ -0,0 +1,26 @@
+import React, { useReducer } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Sidebar from './Sidebar';
+import IState from '../../Interfaces/State';
+import reducers from '../../Reducers/reducers';
+import { Context } from '../../App';
+
+export default {
+  title: 'Sidebar',
+  component: <div>asdad</div>,
+};
+export const Foo = () => <div>asdasdasd</div>;
+
+const initialState = [];
+
+const [state, dispatch] = useReducer<any>(reducers, initialState);
+
+// eslint-disable-next-line react/jsx-no-undef
+const Template: ComponentStory<typeof Sidebar> = (args) => (
+  <Context.Provider value={[]}>
+    <div>hello</div>
+  </Context.Provider>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {};
